fe2da4e34955cab586f3ab36241c64c7954158cc
Keisuke <lakeisuke726@gmail.com>
2026-08-06 13:09:31 +0700

    Update business hours and phone number

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@ document.addEventListener('DOMContentLoaded', () => {
     "Open Daily: 10:00 AM - 9:00 PM": "Buka Setiap Hari: 10:00 AM - 9:00 PM",
     "Phone": "Telefon",
     "Coming Soon": "Akan Datang",
-    "10am - 8pm<br>(OPEN EVERYDAY)": "10am - 8pm<br>(BUKA SETIAP HARI)",
+    "Wed-Thu: 10am - 6pm<br>Fri-Sun: 10am - 7pm": "Rabu-Khamis: 10am - 6pm<br>Jumaat-Ahad: 10am - 7pm",
     "Check Route on Google Maps": "Semak Laluan di Peta Google",
     "Store Exterior": "Luaran Kedai",
     "Store Interior": "Dalaman Kedai",
